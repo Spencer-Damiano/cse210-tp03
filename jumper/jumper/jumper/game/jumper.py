255284285e082bbffd1410fd5302804f0dcfa282
SJD-BYUI <86030230+SJD-BYUI@users.noreply.github.com>
2021-11-09 09:43:24 -0700

    guiding notes
    more notes for methods

---
+++ jumper/jumper/jumper/game/jumper.py
@@ -0,0 +1,16 @@
+
+class Jumper:
+    """
+    init 
+    create jumper
+    """
+
+    def __init__(self):
+        self.num_incorrect = 0
+
+    def create_jumper(self):
+        """
+        if statements according to # of incorrect to decided what text output that we need
+        """
+    
+    
---
+++ jumper/jumper/jumper/game/puzzle.py
@@ -0,0 +1,27 @@
+
+class Puzzle:
+
+    def __init__(self):
+        self.list = []
+        self.current_word = ""
+        self.masked_word = ""
+
+    def chose_word(self):
+        """
+        picks a word
+        """
+    
+    def verify_leter(self):
+        """
+        makes sure the letters are in the word
+        """
+
+    def revel_letter(self):
+        """
+        shows letters
+        """
+    
+    def hide_letter(self):
+        """
+        takes the word and makes the dashes
+        """

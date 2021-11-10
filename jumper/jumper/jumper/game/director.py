f7a95f5eae214f5859a046a87ecb679eafcf243f
Jsantos20201 <72623931+Jsantos20201@users.noreply.github.com>
2021-11-10 09:38:39 -0700

    Update director.py

--- jumper/jumper/jumper/game/director.py
+++ jumper/jumper/jumper/game/director.py
@@ -8,3 +8,18 @@ class Director:
         do outputs
         do updates
     """
+
+    def __init__():
+
+
+
+    def start_game():
+        
+    
+    def do_inputs():
+
+    def do_updates():
+
+    def do_outputs():
+
+    def run_game():
